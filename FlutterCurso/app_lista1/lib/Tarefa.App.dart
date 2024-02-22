7c11160f2647d51432c1448d8bf4acd7a070a4b4
Alberto-MeloL <albertomeloteco@gmail.com>
2024-02-22 16:53:39 -0300

    ok

---
+++ FlutterCurso/app_lista1/lib/Tarefa.App.dart
@@ -0,0 +1,16 @@
+// import 'package:app_lista1/TarefaController.dart';
+// import 'package:app_lista1/TarefaView.dart';
+// import 'package:flutter/material.dart';
+
+// class ListaTarefasApp extends StatelessWidget {
+  
+//   @override
+// Widget build(BuildContext context){
+//   return MaterialApp(
+//     //definindo a tela inicial como a ListaTarefas
+//     home: (context) => ListaTarefasController(),
+//     child: ListaTarefasScreen(),
+//   ),
+// }
+  
+// }
